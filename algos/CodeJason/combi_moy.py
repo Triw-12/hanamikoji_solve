d778951c8d838962300ba84dab204a509a5d67b5
JasonJ13 <jason.jallabert@orange.fr>
2023-12-15 14:55:28 +0100

    fin_15/12

--- algos/CodeJason/combi_moy.py
+++ algos/CodeJason/combi_moy.py
@@ -65,25 +65,25 @@ def combi_to_nmb (lst) :
 
 
 
-def nmb_combi_tour (lst, action) :
-    """" Renvois le nombre de sous manche de lst """
+# def nmb_combi_tour (lst, action) :
+#     """" Renvois le nombre de sous manche de lst """
 
-    nmb_c=combi_to_nmb(lst)
-    total=[0,0,0,0]
+#     nmb_c=combi_to_nmb(lst)
+#     total=0
 
-    if action[0] :
-        total[0] =nmb_c[0]
+#     if action[0] :
+#         total +=nmb_c[0]
 
-    if action[1] :
-        total[1] = nmb_c[1] + (nmb_c[0]*(nmb_c[0]-1))/2
+#     if action[1] :
+#         total += nmb_c[1] + (nmb_c[0]*(nmb_c[0]-1))/2
     
-    if action[2] :
-        total[2] = nmb_c[2] + 2* nmb_c[1]*(nmb_c[0]-1) + 3* (nmb_c[0]-1)*nmb_c[0]*(2*nmb_c[0]-4)/12
+#     if action[2] :
+#         total += nmb_c[2] + 2* nmb_c[1]*(nmb_c[0]-1) + 3* (nmb_c[0]-1)*nmb_c[0]*(2*nmb_c[0]-4)/12
     
-    if action[3] :
-        total[3] = nmb_c[3] + 2* nmb_c[2]*(nmb_c[0]-1) + 3* nmb_c[1]*(nmb_c[1]-1)/2 + 4* nmb_c[1]*( (nmb_c[0]-1) * (nmb_c[0]-2)/2) + 6* nmb_c[0]*(nmb_c[0]-1)* (nmb_c[0]*nmb_c[0]-5*nmb_c[0]+6)/24
+#     if action[3] :
+#         total += nmb_c[3] + 2* nmb_c[2]*(nmb_c[0]-1) + 3* nmb_c[1]*(nmb_c[1]-1)/2 + 4* nmb_c[1]*( (nmb_c[0]-1) * (nmb_c[0]-2)/2) + 6* nmb_c[0]*(nmb_c[0]-1)* (nmb_c[0]*nmb_c[0]-5*nmb_c[0]+6)/24
     
-    return total
+#     return total
 
 
 
@@ -94,7 +94,7 @@ def sous_manche (main, action) :
     nmb_c=combi_to_nmb(main)
     length=len(main)
 
-    sous_manche=[[],[],[],[]]
+    sous_manche=[[],[],[[],[],[]],[[],[],[],[],[]]]
 
     if action[0] :      # Si on choisit la première action
         for i in range (nmb_c[0]) :
@@ -129,33 +129,34 @@ def sous_manche (main, action) :
     if action[2] :      # Si on choisit la troisième action
 
         for i in range (nmb_c[2]) :     # 3 fois la même cartes
-            sous_manche[2].append([])
+            sous_manche[2][0].append([])
+            
 
             for j in range (length) :
                 if j+1>nmb_c[0]+nmb_c[1]+nmb_c[2] or main_tri[nmb_c[0]+nmb_c[1]+i]!=main_tri[j]:
-                    sous_manche[2][-1].append(main_tri[j])
+                    sous_manche[2][0][-1].append(main_tri[j])
         
 
         for i in range (nmb_c[1]) :     # 2 fois la même carte et une carte différente
             for j in range (nmb_c[0]) :
 
                 if main_tri[i+nmb_c[0]] != main_tri[j] :
-                    sous_manche[2].append([])
+                    sous_manche[2][1].append([])
 
                     for c in range (length) :
 
                         if c!=j and (main_tri[c]!=main_tri[i+nmb_c[0]] or c>nmb_c[0]+nmb_c[1]-1) :
-                            sous_manche[2][-1].append(main_tri[c])
+                            sous_manche[2][1][-1].append(main_tri[c])
 
 
         for i in range (nmb_c[0]) :     # 3 cartes différentes
             for j in range (i+1,nmb_c[0]) :
                 for l in range (j+1, nmb_c[0]) :
-                    sous_manche[2].append([])
+                    sous_manche[2][2].append([])
 
                     for c in range (length) :
                         if not appartient([i,j,l],c) :
-                            sous_manche[2][-1].append(main_tri[c])
+                            sous_manche[2][2][-1].append(main_tri[c])
 
 
 
@@ -164,12 +165,12 @@ def sous_manche (main, action) :
 
 
         for i in range (nmb_c[3]) :     # 4 fois la même carte
-            sous_manche[3].append([])
+            sous_manche[3][0].append([])
 
             for c in range (length) :
 
                 if main_tri[c]!=main_tri[i+nmb_a3] or c>=nmb_a3+nmb_c[3] :
-                    sous_manche[3][-1].append(main_tri[c])
+                    sous_manche[3][0][-1].append(main_tri[c])
 
 
         for i in range (nmb_c[2]) :     # 3 fois la même carte et 1 carte différente
@@ -177,21 +178,21 @@ def sous_manche (main, action) :
             for j in range (nmb_c[0]) :
 
                 if main_tri[j] != main_tri[i+nmb_c[0]+nmb_c[1]] :
-                    sous_manche[3].append([])
+                    sous_manche[3][1].append([])
 
                     for c in range (length) :
 
                         if c != j and (main_tri[c] != main_tri[nmb_c[0]+nmb_c[1]+i] or c>=nmb_a3) :
-                            sous_manche[3][-1].append(main_tri[c])
+                            sous_manche[3][1][-1].append(main_tri[c])
 
         for i in range (nmb_c[1]) :     # 2 paires différentes de 2 fois la même carte 
             for j in range (i+1, nmb_c[1]) :
-                sous_manche[3].append([])
+                sous_manche[3][2].append([])
 
                 for c in range (length) :
 
                     if (c>=nmb_c[0]+nmb_c[1] or not(appartient ([main_tri[i+nmb_c[0]],main_tri[j+nmb_c[0]]],main_tri[c]))) :
-                        sous_manche[3][-1].append(main_tri[c])
+                        sous_manche[3][2][-1].append(main_tri[c])
         
         for i in range (nmb_c[1]) :     # 2 fois la même carte et 2 cartes différentes
 
@@ -199,45 +200,66 @@ def sous_manche (main, action) :
                 for l in range (j+1, nmb_c[0]) :
 
                     if main_tri[i+nmb_c[0]]!=main_tri[j] and main_tri[i+nmb_c[0]]!=main_tri[l] :
-                        sous_manche[3].append([])
+                        sous_manche[3][3].append([])
 
                         for c in range (length) :
 
                             if c!=l and c!=j and (c>=nmb_c[0]+nmb_c[1] or main_tri[c]!=main_tri[i+nmb_c[0]]) :
-                                sous_manche[3][-1].append(main_tri[c])
+                                sous_manche[3][3][-1].append(main_tri[c])
         
         for i in range (nmb_c[0]) :     # 4 cartes différentes
             for j in range (i+1,nmb_c[0]) :
                 for l in range (j+1,nmb_c[0]) :
                     for k in range (l+1,nmb_c[0]) :
-                        sous_manche[3].append([])
+                        sous_manche[3][4].append([])
 
                         for c in range (length) :
                             
                             if not(appartient([i,j,l,k], c)) :
-                                sous_manche[3][-1].append(main_tri[c])
+                                sous_manche[3][4][-1].append(main_tri[c])
 
     return sous_manche
 
 def nmb_moy(main, deck, action) :
     """Renvois le nombre de combinaison total sur une partie commenceant par main et deck, avec les actions disponible dans action"""
-    if (action!=[False,False,False,False]):
-        nouv_deck=deck[:]
-        nouv_main=(main+[nouv_deck.pop()])[:]
-        renvois_lst=nmb_combi_tour(main,action)
-        renvois=0
-        ss_m=sous_manche(main,action)
 
-        for i in range (4) :
-            nouv_action=action[:]
-            
-            for lst in ss_m[i] :
-                nouv_action[i]=False
-                renvois += renvois_lst[i]*nmb_moy(nouv_main,nouv_deck,nouv_action)
-        
-        return renvois
-    else :
+    if action==[False,False,False,False] :
         return 1
+
+    nouv_deck=deck [:]
+    nouv_main=(main+ [nouv_deck.pop()]) [:]
+
+    renvois=0
+    ss_m=sous_manche(nouv_main,action)
+
+    for i in range (2) :
+        
+        nouv_action=action[:]
+
+        for lst in ss_m[i] :
+            nouv_action[i]=False
+
+            if action==[False,False,False,False] :
+                return 1
+
+            renvois += nmb_moy(lst,nouv_deck,nouv_action)
+        
+    nouv_action=action[:]
+    nouv_action[2]=False
+
+    for i in range (3) :
+        for lst in ss_m[2][i] :
+            renvois += (i+1)* nmb_moy(lst,nouv_deck,nouv_action)
+
+    nouv_action=action[:]
+    nouv_action[3]=False
+
+    for i in range (5) :
+        for lst in ss_m[3][i] :
+            renvois += (1+i+i//5) * nmb_moy(lst,nouv_deck,nouv_action)
+
+    return renvois
+
 
 
 
@@ -250,4 +272,6 @@ def nmb_moy(main, deck, action) :
 # print(tri_occ(main2))
 # print(sous_manche(main2,[False,False,False,True]))
 
-print(nmb_moy([5, 5, 5, 3, 3, 3],[5,1,1,4],[True,True,True,True]))
+print(nmb_moy([2, 4, 5, 3, 2, 1],[1,6,4,5],[True,True,True,True]))
+
+#print(nmb_combi_tour([5,5,5,4,4,2],[True,True,True,True]))
