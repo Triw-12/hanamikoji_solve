c6c9cb8faaffb34420bef48ee3bdd1c67e5df086
Triw121204 <pagezy.tristan@gmail.com>
2024-02-21 23:44:19 +0100

    Codage de l'algo

---
+++ algos/Algo_dénombrement_finaux/calcul_score.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+int valeur[7] = {2, 2, 2, 3, 3, 4, 5};
+typedef struct double_int
+{
+    int moi; // Sommme
+    int adv; // ponderation
+} D_INT;
+
+typedef struct double_float
+{
+    float som;
+    float pond;
+} D_FLOAT;
+
+bool verif_score_final(int *valide_moi, int *valide_adv);
+
+D_INT score(int *valide_moi, int *valide_adv, int *avantage);
+
+int diff_score(int *valide_moi, int *valide_adv, int *avantage);
+
+D_FLOAT *init_d_float(void);
+
+float ponderation(void);
+
+void ajout(D_FLOAT *simu, int sco);
+
+float total_simu(D_FLOAT *simu);
